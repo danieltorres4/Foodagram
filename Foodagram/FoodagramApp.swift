85c1d116af4b72665b97934fcfb068f215d30ce5
danieltorres4 <dst990502@outlook.com>
2023-03-06 10:47:51 -0600

    Initial Commit

---
+++ Foodagram/FoodagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodagramApp.swift
+//  Foodagram
+//
+//  Created by Iván Sánchez Torres on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
