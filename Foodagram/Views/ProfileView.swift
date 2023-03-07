3ab4991dac93fdc630c7ffd0c0ab1fa6867726e3
danieltorres4 <dst990502@outlook.com>
2023-03-07 12:46:31 -0600

    Added an ImagePicker file to allow the user choose a profile image

--- Foodagram/Views/ProfileView.swift
+++ Foodagram/Views/ProfileView.swift
@@ -12,75 +12,108 @@ struct ProfileView: View {
     @State var expandVerificationWithEmailForm: Bool = false
     @State var textFieldEmail: String = ""
     @State var textFieldPassword: String = ""
+    @State var shouldShowImagePicker: Bool = false
+    @State var image: UIImage?
     
     var body: some View {
-        Form {
-            Section {
-                Text("Username")
-                Text("Email: \(authenticationViewModel.user?.email ?? "No User Logged Yet")")
-            } header: {
-                Label("User Info", systemImage: "person.crop.square.filled.and.at.rectangle")
-            }
-            
-            Section {
-                Button(action: {
-                    expandVerificationWithEmailForm.toggle()
-                }, label: {
-                    Label("Link email and password", systemImage: "envelope.fill")
-                })
-                .disabled(authenticationViewModel.isEmailAndPasswordLinked())
-                
-                if expandVerificationWithEmailForm {
-                    Group {
-                        Text("Link your account with an email and password")
-                            .tint(.secondary)
-                            .multilineTextAlignment(.center)
-                            .padding(.top, 2)
-                            .padding(.bottom, 2)
-                        
-                        TextField("Email", text: $textFieldEmail)
-                        TextField("Password", text: $textFieldPassword)
-                        
-                        Button("Done") {
-                            authenticationViewModel.linkEmailAndPassword(email: textFieldEmail, password: textFieldPassword)
+        NavigationView {
+            Form {
+                Section {
+                    HStack {
+                        Button {
+                            shouldShowImagePicker.toggle()
+                        } label: {
+                            VStack {
+                                if let image = self.image {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .frame(width: 100, height: 100)
+                                        .scaledToFill()
+                                        .clipShape(Circle())
+                                        .cornerRadius(64)
+                                } else {
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 64))
+                                        .padding()
+                                        .foregroundColor(Color.purple)
+                                }
+                            }
                         }
-                        .padding(.top, 18)
-                        .buttonStyle(.bordered)
-                        .tint(.purple)
-                        
-                        if let messageError = authenticationViewModel.messageError {
-                            Text(messageError)
-                                .bold()
-                                .font(.body)
-                                .foregroundColor(.red)
-                                .padding(.top, 20)
+                        VStack {
+                            Text("Username")
+                            Text("Email: \(authenticationViewModel.user?.email ?? "No User Logged Yet")")
                         }
                     }
+
+                    
+                } header: {
+                    Label("User Info", systemImage: "person.crop.square.filled.and.at.rectangle")
                 }
                 
-                Button(action: {
-                    authenticationViewModel.linkFacebook()
-                }, label: {
-                    Label("Link Facebook", systemImage: "f.cursive")
-                })
-                .disabled(authenticationViewModel.isFacebookLinked())
-            } header: {
-                Label("Link other accounts to your session", systemImage: "link")
-            }
-        }
-        .task {
-            authenticationViewModel.getCurrentProvider()
-        }
-        .alert(authenticationViewModel.isLinkedAccount ? "Your account has been linked ✅" : "Error while linking your account ❌", isPresented: $authenticationViewModel.showAlert) {
-            Button("Done") {
-                if authenticationViewModel.isLinkedAccount {
-                    expandVerificationWithEmailForm = false
+                Section {
+                    Button(action: {
+                        expandVerificationWithEmailForm.toggle()
+                    }, label: {
+                        Label("Link email and password", systemImage: "envelope.fill")
+                            .foregroundColor(Color.purple)
+                    })
+                    .disabled(authenticationViewModel.isEmailAndPasswordLinked())
+                    
+                    if expandVerificationWithEmailForm {
+                        Group {
+                            Text("Link your account with an email and password")
+                                .tint(.secondary)
+                                .multilineTextAlignment(.center)
+                                .padding(.top, 2)
+                                .padding(.bottom, 2)
+                            
+                            TextField("Email", text: $textFieldEmail)
+                            TextField("Password", text: $textFieldPassword)
+                            
+                            Button("Done") {
+                                authenticationViewModel.linkEmailAndPassword(email: textFieldEmail, password: textFieldPassword)
+                            }
+                            .padding(.top, 18)
+                            .buttonStyle(.bordered)
+                            .tint(.purple)
+                            
+                            if let messageError = authenticationViewModel.messageError {
+                                Text(messageError)
+                                    .bold()
+                                    .font(.body)
+                                    .foregroundColor(.red)
+                                    .padding(.top, 20)
+                            }
+                        }
+                    }
+                    
+                    Button(action: {
+                        authenticationViewModel.linkFacebook()
+                    }, label: {
+                        Label("Link Facebook", systemImage: "f.cursive")
+                            .foregroundColor(Color.purple)
+                    })
+                    .disabled(authenticationViewModel.isFacebookLinked())
+                } header: {
+                    Label("Link other accounts to your session", systemImage: "link")
                 }
             }
-        } message: {
-            Text(authenticationViewModel.isLinkedAccount ? "Succesfully linked account" : "Failure")
+            .task {
+                authenticationViewModel.getCurrentProvider()
+            }
+            .alert(authenticationViewModel.isLinkedAccount ? "Your account has been linked ✅" : "Error while linking your account ❌", isPresented: $authenticationViewModel.showAlert) {
+                Button("Done") {
+                    if authenticationViewModel.isLinkedAccount {
+                        expandVerificationWithEmailForm = false
+                    }
+                }
+            } message: {
+                Text(authenticationViewModel.isLinkedAccount ? "Succesfully linked account" : "Failure")
+            }
         }
-
+        .fullScreenCover(isPresented: $shouldShowImagePicker) {
+            ImagePicker(image: $image)
+        }
     }
 }
 
